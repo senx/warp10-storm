5a705b4f8f3194a10bc24f38fe2d127b0256c07a
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2018-11-03 14:31:46 +0100

    Fixed copyright notice

--- src/main/java/io/warp10/storm/ext/StormMqttSpout.java
+++ src/main/java/io/warp10/storm/ext/StormMqttSpout.java
@@ -1,3 +1,18 @@
+//
+//   Copyright 2018  SenX S.A.S.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
 package io.warp10.storm.ext;
 
 import io.warp10.script.WarpScriptStack;
@@ -150,7 +165,8 @@ public class StormMqttSpout implements IRichSpout, Snapshotable {
       SNAPSHOT.addElement(sb, MQTTSPOUT.KEY_topics);
       SNAPSHOT.addElement(sb, this.subscriptions);
       sb.append("} ");
-      sb.append("MQTTSPOUT ");
+      sb.append(MQTTSPOUT.MQTTSPOUT);
+      sb.append(" ");
     } catch (Exception e) {      
     }
     return sb.toString();
